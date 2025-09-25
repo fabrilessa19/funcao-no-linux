2c033c5fd7febd6e3b70d8ac448b38abbe00137b
fabrilessa19 <pedrobraboshiny@gmail.com>
2025-09-25 13:14:23 -0300

    Update funcao.sh

--- funcao.sh
+++ funcao.sh
@@ -1,6 +1,5 @@
 -este comando serve para mostrar os 10 processos que mais consomem memoria.
-
-procs_mem() {
+cpu_mem() {
     echo "10 processos que mais consomem Memória:"
     ps -eo pid,comm,%cpu,%mem --sort=-%mem | head -n 11
 }
